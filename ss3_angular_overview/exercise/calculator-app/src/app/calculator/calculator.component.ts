6aa9747cb3eb6f04ee79ca02765ef0ef763116b4
Your Name <you@example.com>
2022-08-15 15:05:05 +0700

    commit

--- ss3_angular_overview/exercise/calculator-app/src/app/calculator/calculator.component.ts
+++ ss3_angular_overview/exercise/calculator-app/src/app/calculator/calculator.component.ts
@@ -1,36 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
+
 export class CalculatorComponent implements OnInit {
+  result: number = 0 ;
 
-  public num1: number;
-  public num2: number;
-  public result: number;
-
-  constructor() {
-  }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  add(): void {
-    this.result = Number(this.num1) + Number(this.num2);
-  }
-
-  sub(): void {
-    this.result = Number(this.num1) - Number(this.num2);
-  }
-
-  mul(): void {
-    this.result = Number(this.num1) * Number(this.num2);
-  }
-
-  div(): void {
-    this.result = Number(this.num1) / Number(this.num2);
 
   }
+
+  public math(idNumber1: any , idNumber2: any , idMath: any) {
+    console.log(idMath);
+    switch (idMath) {
+      case "summation" :
+        this.result = Number(idNumber1) + Number(idNumber2);
+        break;
+      case "subtraction" :
+        this.result = idNumber1 - idNumber2;
+        break;
+      case "multiplication" :
+        this.result = idNumber1 * idNumber2;
+        break;
+      case "division" :
+        this.result = idNumber1 / idNumber2;
+        break;
+
+    }
+  }
+
 }
