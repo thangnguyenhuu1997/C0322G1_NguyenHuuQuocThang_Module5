5a0d9413baa784bdb55f21b8a40cd81d1c0f685c
Your Name <you@example.com>
2022-08-17 14:20:54 +0700

    commit

--- ss5_component_interaction/practice/progress-bar/src/app/progress-bar/progress-bar.component.ts
+++ ss5_component_interaction/practice/progress-bar/src/app/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress-bar.component.css']
 })
 export class ProgressBarComponent implements OnInit {
+  @Input() backgroundColor = '#D9D9D9';
+  @Input() progressColor = '#4CAF50';
+  @Input() progress = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.loading();
+  }
+
+  loading() {
+    let interval = setInterval(() => {
+      this.progress = +this.progress + 5;
+      if (this.progress == 100) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 }
